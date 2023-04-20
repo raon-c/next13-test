af34c4e78bc4ce8c4981c265623ad63ec1b94358
raon.c <raon.c@kakaoent.com>
2023-04-20 12:15:13 +0900

    tailwind 적용

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,8 @@
+export default function AuthLayout({ children }) {
+  return (
+    <>
+      <span>this is auth layout</span>
+      {children}
+    </>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import "../styles/Home.module.css";
+import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
